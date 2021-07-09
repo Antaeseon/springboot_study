765145a4ec91f9d0dcaddcbd06fd5a46bf9ccaed
Antaeseon <ants412@naver.com>
2021-07-10 02:31:31 +0900

    Add Order logic

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
 
+    // 동시성 이슈가 생길 수 있기 때문에 ConcurrentHashMap을 사용해야 한다.
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
