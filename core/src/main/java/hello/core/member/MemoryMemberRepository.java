24aaee0f310e31a53d60570ddab1b92d22136187
Antaeseon <ants412@naver.com>
2021-07-16 01:09:31 +0900

    add AutoAppConfig scenario

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 이슈가 생길 수 있기 때문에 ConcurrentHashMap을 사용해야 한다.
