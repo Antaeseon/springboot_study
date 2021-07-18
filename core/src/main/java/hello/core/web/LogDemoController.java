8eb185bb248e582513658d6629c0f07055a49fc1
Antaeseon <ants412@naver.com>
2021-07-19 00:17:30 +0900

    finish inflearn lec

---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final  MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String longDeo(HttpServletRequest request) {
+        //이렇게 사용하지 않고 생성자로 처리를 하면 오류가 난다 ->
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
