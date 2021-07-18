35aca55de1203fd8dc4aa07a41be084975960c66
Antaeseon <ants412@naver.com>
2021-07-18 15:26:33 +0900

    Primary, Qualifier Annotation.

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,25 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
 
+
+    // final을 쓰면 생성자를 통해 설정을 안 할 경우 오류가 나서 문제 원인을 파악하는데 효과적이다.
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy ;
+    private final DiscountPolicy discountPolicy;
+
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) { //command option + B 구현체 검색
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
