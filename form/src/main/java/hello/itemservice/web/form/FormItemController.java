73267d2590e84babdea523526dbe5728f4c7dfb7
Antaeseon <ants412@naver.com>
2021-09-05 16:56:34 +0900

    clear thymelear intergration!

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Slf4j
@@ -19,6 +24,35 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+//어떤게 호출이 되던 모두 이 내역이 포함이 된다.
+    @ModelAttribute("regions")
+    public Map<String,String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        //COMMAND + OPTION + N => 한줄로 합쳐준다.
+        return ItemType.values();
+    }
+
+
+    // 이는 미리 생성해놓고 사용하는 것이 더 효율적이다. 예시를 위해서 이렇게 표시
+    // static으로 생성 후 공유 필요
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode(("FAST"), "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode(("NORMAL"), "일반 배송"));
+        deliveryCodes.add(new DeliveryCode(("SLOW"), "느린 배송"));
+        return deliveryCodes;
+    }
+
 
     @GetMapping
     public String items(Model model) {
@@ -43,6 +77,8 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
