e08464e60a1851c2e3ca716fec4f14ceca166ed9
Antaeseon <ants412@naver.com>
2021-12-24 01:26:11 +0900

    feat: LogTrace without TraceId

--- advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -1,8 +1,7 @@
 package hello.advanced.app.v2;
 
-import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +10,13 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
-    private final HelloTraceV2 trace;
+    private final LogTrace trace;
 
-    public void orderItem(TraceId traceId, String itemId) {
+    public void orderItem( String itemId) {
         TraceStatus status = null;
         try {
-            status = trace.beginSync(traceId, "OrderService.orderItem()");
-            orderRepository.save(status.getTraceId(), itemId);
+            status = trace.begin( "OrderService.orderItem()");
+            orderRepository.save(itemId);
             trace.end(status);
         } catch (Exception e) {
             trace.exception(status, e);
