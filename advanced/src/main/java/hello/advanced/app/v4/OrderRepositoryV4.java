fc4bf8a6d0b8eb2dc158e2e00b5716322fb86a97
Antaeseon <ants412@naver.com>
2022-01-06 23:57:52 +0900

    feat: AbstractTemplate V4

---
+++ advanced/src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,42 @@
+package hello.advanced.app.v4;
+
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()"); // cmd + option + n => inline variable
+
+    }
+
+    private void sleep(int mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
