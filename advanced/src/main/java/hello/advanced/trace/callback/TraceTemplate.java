9f13ab3351848e8d812a0dd24d7f22b0763d8f38
Antaeseon <ants412@naver.com>
2022-01-11 02:25:29 +0900

    feat: strategy pattern v3

--- advanced/src/main/java/hello/advanced/trace/callback/TraceTemplate.java
+++ advanced/src/main/java/hello/advanced/trace/callback/TraceTemplate.java
@@ -1,23 +1,23 @@
-package hello.advanced.trace.template;
+package hello.advanced.trace.callback;
 
 import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.logtrace.LogTrace;
 
-public abstract class AbstractTemplate<T> {
+public class TraceTemplate {
 
     private final LogTrace trace;
 
-    public AbstractTemplate(LogTrace trace) {
+    public TraceTemplate(LogTrace trace) {
         this.trace = trace;
     }
 
-    public T execute(String message) {
+    public <T> T execute(String message, TraceCallback<T> callback) {
         TraceStatus status = null;
         try {
             status = trace.begin(message);
 
             //로직 호출
-            T result = call();
+            T result = callback.call();
 
             trace.end(status);
             return result;
@@ -25,7 +25,6 @@ public abstract class AbstractTemplate<T> {
             trace.exception(status, e);
             throw e; // 예외를 꼭 다시 던져주어야 한다.
         }
-    }
 
-    protected abstract T call();
+    }
 }
