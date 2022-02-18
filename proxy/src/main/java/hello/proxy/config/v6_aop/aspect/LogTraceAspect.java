7accf28984641b1a94dcadcdbf2680d80ae680b0
Antaeseon <ants412@naver.com>
2022-02-18 23:58:41 +0900

    feat: LogTraceAspect

--- proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -1,4 +1,13 @@
 package hello.proxy.config.v6_aop.aspect;
 
+import hello.proxy.trace.logtrace.LogTrace;
+
 public class LogTraceAspect {
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+
 }
