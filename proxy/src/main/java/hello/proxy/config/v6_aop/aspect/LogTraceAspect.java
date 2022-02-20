3c1f8226d9ee983f5c69e954229acdb8358c5c71
Antaeseon <ants412@naver.com>
2022-02-20 23:57:15 +0900

    feat: TraceStatus

--- proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -1,7 +1,9 @@
 package hello.proxy.config.v6_aop.aspect;
 
+import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 
 public class LogTraceAspect {
     private final LogTrace logTrace;
@@ -10,9 +12,14 @@ public class LogTraceAspect {
         this.logTrace = logTrace;
     }
 
+
+    @Around("execution(* hello.proxy.app..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
 
-        return null;
+        TraceStatus status = null;
+
+
+        return status;
     }
 
 }
